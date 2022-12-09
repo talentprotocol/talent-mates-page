44ff3c93ecebe805d7400cb3269cfbb424f3d384
bguedes <bruno.guedes@mozantech.com>
2022-12-09 17:31:42 +0100

    fixed max trait issue on shuffle

--- components/nft-picker/hooks/use-trait/index.ts
+++ components/nft-picker/hooks/use-trait/index.ts
@@ -81,7 +81,7 @@ export const useTrait = ({ name, gender, maxElements, description }: Props) => {
 
 	const shuffle = useCallback(() => {
 		const newSelection = Math.floor(
-			Math.random() * (maxElements[gender] + 1) +
+			Math.random() * (maxElements[gender]) +
 				// @ts-ignore(
 				(MANDATORY_PROPERTIES_LIST[name] ? 1 : 0)
 		);
