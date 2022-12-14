27c3f1b3ef750bdde9c63b719c013253491537b7
francisco-leal <francisco@talentprotocol.com>
2022-12-14 09:16:50 +0000

    Add social media descriptions for sharing

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -32,7 +32,7 @@ const Home: NextPage = () => {
 	return (
 		<>
 			<Head>
-				<title>Talent Mates</title>
+				<title>Talent Mates NFTs by Talent Protocol</title>
 				<meta
 					name="description"
 					content="Talent Mates is a new NFT avatar collection, exclusive for the Talent Protocol community. Mint your NFT to enter a world where both talent and opportunities are abundant."
@@ -42,6 +42,21 @@ const Home: NextPage = () => {
 					content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover"
 				></meta>
 				<link rel="icon" href="/favicon.ico" />
+				<meta
+          property="og:url"
+          content="https://mates.talentprotocol.com"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Talent Mates NFTs by Talent Protocol"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="og:description"
+          content="Talent Mates is a new NFT avatar collection, exclusive for the Talent Protocol community. Mint your NFT to enter a world where both talent and opportunities are abundant."
+        />
+				<meta property="og:image" content="/metaimage.jpg" />
 			</Head>
 			<Modal
 				isOpen={modalState.isOpen}
