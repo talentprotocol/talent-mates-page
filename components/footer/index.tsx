af57a3bee78bf15fc10722ef853f4f413d965aa7
francisco-leal <francisco@talentprotocol.com>
2022-12-14 09:35:42 +0000

    Update footer with missing links and remove Talent Protocol T&C

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -18,6 +18,14 @@ export const Footer = ({ fixed = false }) => (
 				<a
 					target="_blank"
 					rel="noreferrer"
+					href="https://www.talentprotocol.com/"
+				>
+					Talent Protocol
+				</a>
+				<div />
+				<a
+					target="_blank"
+					rel="noreferrer"
 					href="https://talentprotocol.notion.site/Talent-Protocol-101-26a6c4b9f8284e5e8f55603a38629e76"
 				>
 					About
@@ -50,17 +58,9 @@ export const Footer = ({ fixed = false }) => (
 				<a
 					target="_blank"
 					rel="noreferrer"
-					href="https://talentprotocol.notion.site/Terms-Conditions-ec060cb6c06e49a98f17d235d0835773"
-				>
-					T&C
-				</a>
-				<div />
-				<a
-					target="_blank"
-					rel="noreferrer"
 					href="https://www.notion.so/talentprotocol/Talent-Mates-Terms-Conditions-a9a81a33fab5406ea496c4868fe2d830"
 				>
-					Mates - T&C
+					Terms
 				</a>
 			</section>
 		</InnerContainer>
