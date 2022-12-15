97a1e724ed7fa76dfd43bebbc56410a9c45f2898
francisco-leal <francisco@talentprotocol.com>
2022-12-15 11:32:35 +0000

    Change metamask message

--- components/nft-picker/index.tsx
+++ components/nft-picker/index.tsx
@@ -36,7 +36,7 @@ ContractBook.new = {
 	chainId: "44787",
 };
 
-const AUTH_SIGNED_MESSAGE = "I'm signing this message";
+const AUTH_SIGNED_MESSAGE = "Sign this message for us to guarantee that you are the owner of the NFT and can change it's metadata.";
 const CANVAS_SIDE = 569;
 const BASE_URI =
 	"ipfs://bafyreifuc7inyu6fhytj2vof6qhrejkla7ohd7qwac33gfwcc57mrbxfn4/metadata.json";
