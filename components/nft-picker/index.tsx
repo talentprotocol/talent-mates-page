c50ae74e68d73dcef228126c45107ff4d1e6a317
francisco-leal <francisco@talentprotocol.com>
2022-12-15 01:13:30 +0000

    Change to new contract

--- components/nft-picker/index.tsx
+++ components/nft-picker/index.tsx
@@ -31,7 +31,7 @@ import { ContractBook } from "libs/contract-book";
 ContractBook.new = {
 	name: "TalentNFT",
 	abi: abi.abi,
-	address: "0x651cf1667307451537c6687649cB9bf4833072e3",
+	address: "0x8193958d27A0788C129703515751522fd0a1A7FC",
 	network: "https://alfajores-forno.celo-testnet.org",
 	chainId: "44787",
 };
