706a09cb284d66feee09e7a31a203288f45bd982
francisco-leal <francisco@talentprotocol.com>
2023-05-24 14:00:43 +0100

    Add google analytics to talent mates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,30 @@ import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { GlobalStyles } from "global-styles";
 import "animate.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<GlobalStyles />
-			<Component {...pageProps} />
-			<Analytics />
-		</>
-	);
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="GoogleAnalytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+      <GlobalStyles />
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
